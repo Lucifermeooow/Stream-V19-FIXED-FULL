2c264d4211b6019f8752f701304a8149f0916e9f
Lucifermeooow <119759050+Lucifermeooow@users.noreply.github.com>
2026-08-29 06:02:11 +0300

    Refactor widget test for Stream V19
    Updated widget test to use MyApp instead of StreamV19App and changed expectations.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,9 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:stream_v19/main.dart';
 
 void main() {
-  testWidgets('Stream V19 renders', (tester) async {
-    await tester.pumpWidget(const StreamV19App());
-    expect(find.text('Stream V19'), findsOneWidget);
-    expect(find.text('GO LIVE'), findsOneWidget);
+  testWidgets('Stream V19 renders', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    expect(find.byType(MyApp), findsOneWidget);
   });
 }
